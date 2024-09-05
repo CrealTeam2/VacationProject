72074cdd06d3bd204613012666a2de7429dcefeb
roy0206 <swimkinig_ryu@naver.com>
2024-09-06 04:34:58 +0900

    인게임씬 1 수정

--- VacationProject2024/Assets/Scripts/Zombie/NavmeshBaker.cs
+++ VacationProject2024/Assets/Scripts/Zombie/NavmeshBaker.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavmeshBaker : Singleton<NavmeshBaker>
 {
+    Transform player; 
     NavMeshSurface surface;
     private float curTime = 0;
     [SerializeField] float coolTime;
 
     private void Awake()
     {
+
         surface = GetComponent<NavMeshSurface>();
         surface.BuildNavMesh();
+        player = GameObject.FindWithTag("Player").transform;
+        BakeNavmesh();
     }
 
     private void Update()
@@ -20,8 +25,15 @@ public class NavmeshBaker : Singleton<NavmeshBaker>
         curTime += Time.deltaTime;
         if(curTime > coolTime)
         {
-            surface.BuildNavMesh();
+            BakeNavmesh();
             curTime = 0;
         }
+    }
+
+    void BakeNavmesh()
+    {
+        surface.size = new Vector3(20, 20, 20);
+        surface.center = player.position;
+        surface.BuildNavMesh();
     }
 }
