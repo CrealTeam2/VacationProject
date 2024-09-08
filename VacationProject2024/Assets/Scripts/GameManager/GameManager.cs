039dbf9adeb096868323f14bf6f405f34e95f139
roy0206 <swimkinig_ryu@naver.com>
2024-09-08 18:28:37 +0900

    사소한 수정

--- VacationProject2024/Assets/Scripts/GameManager/GameManager.cs
+++ VacationProject2024/Assets/Scripts/GameManager/GameManager.cs
@@ -21,8 +21,8 @@ public class GameManager : Singleton<GameManager>, ISavable, ISingletonStart
     {
         Init();
         Application.targetFrameRate = 60;
-
     }
+
     void Init()
     {
         savePoints = new();
