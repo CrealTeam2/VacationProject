558df4a28f0509045df2f55913a122093fe71eb1
roy0206 <swimkinig_ryu@naver.com>
2024-09-10 23:26:23 +0900

    .

--- VacationProject2024/Assets/Scripts/Activation/Activators/Activator_Trigger.cs
+++ VacationProject2024/Assets/Scripts/Activation/Activators/Activator_Trigger.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Activator_Trigger : Activator
 {
+    bool t = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && t == false)
         {
             Activate();
             SoundManager.Instance.PlaySound(gameObject, "Scream", 1, 1);
+            t = true;
         }
     }
 }
